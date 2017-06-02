c65c6dc106d5d9969d91ebaf87017f7ec45b9f3b
Admin <admin@Mac-Admin.local>
2017-06-02 08:12:26 +0700

    Yet changes

--- HearMeNow/ViewController.swift
+++ HearMeNow/ViewController.swift
@@ -47,6 +47,8 @@ class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDe
             do {
                 try soundPlayer = AVAudioPlayer(contentsOf: url)
                 soundPlayer?.delegate = self
+                soundPlayer?.enableRate = true
+                soundPlayer?.rate = 0.5
                 soundPlayer?.play()
             } catch {
                 print("Error initializing player \(error)")
